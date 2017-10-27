155a64a3cf78bf93ee798690a59699a862fcb6a8
Jacek Arciszewski <arciszewski.jm@gmail.com>
2017-10-27 02:44:20 +0200

    Updated Player class with needed properties.

--- CodingKobold.LFG/CodingKobold.LFG.Domain/Entities/Player.cs
+++ CodingKobold.LFG/CodingKobold.LFG.Domain/Entities/Player.cs
@@ -1,12 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CodingKobold.LFG.Domain.Entities
 {
+    public enum Gender { Male, Female }
+
     public class Player
     {
+        public string Nickname { get; set; }
+
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public Gender Gender { get; set; }
         public string City { get; set; }
+
+        public byte[] Avatar { get; set; }
+        public string Description { get; set; }
+
+        public DateTime RegisterTime { get; set; }
+        public DateTime LastSeenTime { get; set; }
 
         public List<RpgSystem> RpgSystems { get; set; }
 
