7b6b62a215cc23397433491de44dff3ef01f3f78
Jacek Arciszewski <arciszewski.jm@gmail.com>
2017-11-03 01:10:58 +0100

    Added sample unit test project for player class.

---
+++ CodingKobold.LFG/CodingKobold.LFG.Domain.UnitTests/Entities/PlayerTests.cs
@@ -0,0 +1,23 @@
+﻿using CodingKobold.LFG.Domain.Entities;
+using NUnit.Framework;
+
+namespace CodingKobold.LFG.Domain.UnitTests.Entities
+{
+    [TestFixture]
+    public class PlayerTests
+    {
+        [Test]
+        public void MethodName_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var result = 2;
+
+            // Act
+            result += 2;
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+    }
+}
