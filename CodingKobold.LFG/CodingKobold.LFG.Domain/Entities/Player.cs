1330dd244102aa3f759abe468f6a8f5cda8a95de
Jacek Arciszewski <arciszewski.jm@gmail.com>
2017-11-17 14:15:11 +0100

    Commented Gender in Player class.

--- CodingKobold.LFG/CodingKobold.LFG.Domain/Entities/Player.cs
+++ CodingKobold.LFG/CodingKobold.LFG.Domain/Entities/Player.cs
@@ -11,7 +11,7 @@ namespace CodingKobold.LFG.Domain.Entities
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Gender Gender { get; set; }
+        //public Gender Gender { get; set; }
         public string City { get; set; }
 
         //public LoginInfo LoginInfo { get; set; }
