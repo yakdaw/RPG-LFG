16ed66b2c7655bbda1acb485bbf88a934a79af84
Jacek Arciszewski <arciszewski.jm@gmail.com>
2017-11-17 14:09:51 +0100

    Added DbContext, simplified Player class.

---
+++ CodingKobold.LFG/CodingKobold.LFG.API/Persistence/RpgLfgContext.cs
@@ -0,0 +1,20 @@
+﻿using CodingKobold.LFG.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingKobold.LFG.API.Persistence
+{
+    public class RpgLfgContext : DbContext
+    {
+        public RpgLfgContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        public DbSet<Player> Players { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO : Fluent API
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
